3cfdee1bfc1966c1b732595ada5b36e6e4811c9c
Dragonhielo199 <usuario2@gmail.com>
2026-04-09 13:17:15 +0200

    Modificación del comentario explicativo de porqué main está vacía

--- src/main/java/es/tienda/modelos/Principal.java
+++ src/main/java/es/tienda/modelos/Principal.java
@@ -2,6 +2,6 @@ package es.tienda.modelos;
 
 public class Principal {
     public static void main(String[] args) {
-        // Esperando a que mis compañeros terminen
+        // A la espera
     }
 }
