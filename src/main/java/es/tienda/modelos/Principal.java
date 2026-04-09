0123f8f741539dfda25f229010745d17af18d2ab
Dragonhielo199 <usuario2@gmail.com>
2026-04-09 13:14:34 +0200

    Vuelta al estado original

--- src/main/java/es/tienda/modelos/Principal.java
+++ src/main/java/es/tienda/modelos/Principal.java
@@ -2,9 +2,6 @@ package es.tienda.modelos;
 
 public class Principal {
     public static void main(String[] args) {
-
-        ArrayList<Cliente> clientes = new ArrayList<>();
-        ArrayList<Proveedor> proveedores = new ArrayList<>();
-        ArrayList<Articulo> articulos = new ArrayList<>();
+        // Esperando a que mis compañeros terminen
     }
 }
